a8599d81b72460bbbf529b0feec41938ba4c088c
oroboi320 <153698236+oroboi320@users.noreply.github.com>
2024-02-08 10:48:21 +0800

    Update script.js
    update jscript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        var allCombinations = [];
+  var allCombinations = [];
 
      function applyRules(digit) {
             switch (digit) {
@@ -13,7 +13,7 @@
                 case '4':
                     return ['7', '9', '4', '5', '3', '0', '1'];
                 case '5':
-                    return ['2', '3', '5', '4'];
+                    return ['2', '3', '5', '4', '6'];
                 case '6':
                     return ['9', '6', '5', '7'];
                 case '7':
@@ -51,7 +51,38 @@
                 alert("Please enter a valid 3-digit number.");
             }
         }
+function filterCombinations2() {
+    var filterValue = document.getElementById("filterBox2").value.toUpperCase();
 
+    if (/^[EO]{3}$/.test(filterValue)) {
+        if (filterValue.length === 3) {
+            var filteredCombinations2 = allCombinations.filter(function(combination) {
+                var oddCount = 0;
+                var evenCount = 0;
+
+                for (var i = 0; i < 3; i++) {
+                    var digit = combination[i];
+
+                    if (['0', '2', '4', '6', '8'].includes(digit)) {
+                        evenCount++;
+                    } else if (['1', '3', '5', '7', '9'].includes(digit)) {
+                        oddCount++;
+                    }
+                }
+
+                return oddCount === filterValue.split('O').length - 1 &&
+                       evenCount === filterValue.split('E').length - 1;
+            });
+
+            displayCombinations(filteredCombinations2);
+        } else {
+            // If the filter box is empty or doesn't have exactly 3 characters, display all combinations
+            displayCombinations(allCombinations);
+        }
+    } else {
+        alert("Please enter a valid 3-digit format using 'E' and/or 'O' in the filter box.");
+    }
+}
         function filterCombinations() {
             var filterValue = document.getElementById("filterBox").value;
 
@@ -86,3 +117,48 @@
                 generatedCombinationsElement.appendChild(combinationElement);
             }
         }
+function filterCombinations3() {
+    var filterValue = document.getElementById("filterBox3").value.toUpperCase();
+
+    if (/^[HL]{3}$/.test(filterValue)) {
+        if (filterValue.length === 3) {
+            var filteredCombinations3 = allCombinations.filter(function(combination) {
+                var highCount = 0;
+                var lowCount = 0;
+
+                for (var i = 0; i < 3; i++) {
+                    var digit = combination[i];
+
+                    if (['5', '6', '7', '8', '9'].includes(digit)) {
+                        highCount++;
+                    } else if (['0', '1', '2', '3', '4'].includes(digit)) {
+                        lowCount++;
+                    }
+                }
+
+                return highCount === filterValue.split('H').length - 1 &&
+                       lowCount === filterValue.split('L').length - 1;
+            });
+
+            displayCombinations(filteredCombinations3);
+        } else {
+            // If the filter box is empty or doesn't have exactly 3 characters, display all combinations
+            displayCombinations(allCombinations);
+        }
+    } else {
+        alert("Please enter a valid 3-digit format using 'H' and/or 'L' in the filter box.");
+    }
+}
+function chooseLuckyCombination() {
+        // Check if there are generated combinations
+        if (allCombinations.length > 0) {
+            // Randomly choose a combination
+            var randomIndex = Math.floor(Math.random() * allCombinations.length);
+            var luckyCombination = allCombinations[randomIndex];
+
+            // Display the lucky combination
+            alert("Your Lucky Combination is: " + luckyCombination + ";" +  " Always play rambolito ");
+        } else {
+            alert("Generate probables first before choosing a lucky combination.");
+        }
+    }
